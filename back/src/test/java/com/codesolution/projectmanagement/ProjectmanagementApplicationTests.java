6d6572e3b10961ac00c6c58c4e905a747bf43d8f
Allan Pure <allan@pure-illusion.com>
2025-04-02 12:20:44 +0200

    Ajout d'un docker file +docker compose pour lancer le back

--- back/src/test/java/com/codesolution/projectmanagement/ProjectmanagementApplicationTests.java
+++ back/src/test/java/com/codesolution/projectmanagement/ProjectmanagementApplicationTests.java
@@ -1,21 +1,9 @@
 package com.codesolution.projectmanagement;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-
 @SpringBootTest
-class ProjectmanagementApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void mainMethodTest() {
-		// Vérifie que la méthode main ne lance pas d'exception
-		assertDoesNotThrow(() -> ProjectmanagementApplication.main(new String[] {}));
-	}
-
-}
+class ProjectmanagementApplicationTests {}
